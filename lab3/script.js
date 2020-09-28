e7b6e947f33c92fef3799a9728b02889eee9b1eb
Patricia Acorda <acordp@rpi.edu>
2020-09-28 01:45:02 -0400

    Lab3 - acordp.html finished

---
+++ lab3/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() { 
+
+    // toggles display of subsections on acordp.html
+    $("#acordpPage nav a").click (function() {
+        var linkTitle = $(this).attr("class");
+        var currDisplay = $("#acordpPage div." + linkTitle).css("display");
+        if (currDisplay == "block") {
+            $(this).css("color", "var(--dark-color2-pa)");
+            $("#acordpPage div." + linkTitle).css("display", "none");
+            $("#acordpPage #aboutMe").css("display", "block");
+        }
+        else {
+            $("#acordpPage nav a").css("color", "var(--dark-color2-pa)");
+            $("#acordpPage #aboutMe").css("display", "none");
+            $("#acordpPage div.Education").css("display", "none");
+            $("#acordpPage div.WorkExperience").css("display", "none");
+            $("#acordpPage div.Skills").css("display", "none");
+
+            $("#acordpPage div." + linkTitle).css("display", "block");
+            $(this).css("color", "var(--light-color2-pa)");
+        }
+    });
+
+    
+
+});
