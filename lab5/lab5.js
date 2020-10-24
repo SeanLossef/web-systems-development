456e024533c0e4440c6be1d5cf977c0bd41a4881
Patricia Acorda <acordp@rpi.edu>
2020-10-23 23:14:38 -0400

    Outline + simple info bar
    getting the hang of jquery plugins

---
+++ lab5/lab5.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $.fn.hexed = function(settings) {
+        // default settings
+        var opts = $.extend( {}, $.fn.hexed.defaults, settings );
+        
+        $("#name").text(opts.playerName);
+        $("#turns").text(opts.turnNum);
+        
+    
+    };
+    // Define plugin defaults
+    $.fn.hexed.defaults = {
+        playerName: "YourName",
+        turnNum: 3
+    };
+
+    // Run the plugin
+    $("#hexedDiv").hexed("somejson");
+});
+
